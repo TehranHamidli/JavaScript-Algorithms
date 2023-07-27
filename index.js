f1852845e305e0926e2bcf7a999357fe726febd3
Your Name <you@example.com>
2023-07-27 18:35:05 +0400

    JavaScript Algorthms

--- index.js
+++ index.js
@@ -196,3 +196,28 @@ Result: 2
 // }
 
 // hamming('Hello World', "Hello World")
+
+//Task 11
+
+/* Write a function that when given a URL as a string,
+ parses out just the domain name and returns it as a string.
+ 
+For example:
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet"
+
+*/
+
+// function domainName(url) {
+//   url = url.replace("http://", '');
+//   url = url.replace("https://", '');
+//   url = url.replace("www.", '');
+
+//   return url.split(".")[0];
+
+// }
+
+// let domain = domainName("http://github.com/carbonfive/raygun");
+
+// console.log(domain);
