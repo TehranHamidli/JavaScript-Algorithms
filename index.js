a5f1e626541d8ab745e5fdafe7c483ce58c0c0cb
Your Name <you@example.com>
2023-07-27 17:46:06 +0400

    JavaScript Algoritrms

--- index.js
+++ index.js
@@ -166,4 +166,33 @@ twoArePositive(-14, -3, -4) == false
 //     }
 // }
 
+// Task 10
 
+/* The Hamming Distance is a measure of similarity between two strings of equal length.
+ Complete the function so that it returns the number of positions where the input strings do not match. 
+Examples:
+a = "I like turtles"
+b = "I like turkeys"
+Result: 3
+
+a = "Hello World"
+b = "Hello World"
+Result: 0
+
+a = "espresso"
+b = "Expresso"
+Result: 2
+
+*/
+
+// function hamming(a, b) {
+//     let sehvSayi = 0;
+//     for (let i = 0; i < a.length; i++) {
+//         if (a[i] !== b[i]) {
+//            sehvSayi++
+//         }
+//     }
+//     console.log(sehvSayi);
+// }
+
+// hamming('Hello World', "Hello World")
